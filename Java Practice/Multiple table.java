e85d98a25a04558ff416734f54d0f498624ba687
Fahim Al-Amin Auntu <123312309+auntu4672@users.noreply.github.com>
2023-02-15 23:25:03 +0600

    Update Multiple table.java

--- Java Practice/Multiple table.java
+++ Java Practice/Multiple table.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner number = new Scanner (System.in);
+      System.out.println ("Enter a number= ");
+    int a = number.nextInt ();
+    for (int i = 1; i <= 10; i++)
+      {
+	System.out.println ("i*a= " + i * +a);
+      }
+
+  }
+}
