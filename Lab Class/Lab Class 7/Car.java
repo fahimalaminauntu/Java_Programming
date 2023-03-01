55f6f3c6a0d30cf9a64ba7c07068e764918ac111
Fahim Al-Amin Auntu <123312309+auntu4672@users.noreply.github.com>
2023-03-01 12:42:14 +0600

    Overloading 2.0

--- Lab Class/Lab Class 7/Car.java
+++ Lab Class/Lab Class 7/Car.java
@@ -1,1 +1,14 @@
 
+import java.util.Scanner;
+
+public class Car extends Vehicle {
+
+    public void run() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Car Speed  ");
+        speed = input.nextInt();
+        System.out.println("Car Speed is :" + speed);
+        input.close();
+    }
+
+}
