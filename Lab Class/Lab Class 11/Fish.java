534ba4af52088df594c10366fc96742a079ed0c6
Fahim Al-Amin Auntu <123312309+auntu4672@users.noreply.github.com>
2023-06-07 02:07:02 +0600

    Child Class

---
+++ Lab Class/Lab Class 11/Fish.java
@@ -0,0 +1,27 @@
+public class Fish extends Animal implements pet {
+    public Fish() {
+
+    }
+
+    public void getName() {
+
+    }
+
+    public void setName() {
+
+    }
+
+    public void play() {
+
+    }
+
+    public void walk() {
+        super.walk(legs);
+
+    }
+
+    public void eat() {
+
+    }
+
+}
